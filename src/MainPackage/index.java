f35e41413e0a2e3b9a66333d5000dedf67bc255f
Daryassatya <daryassatya@gmail.com>
2022-01-19 12:57:14 +0700

    Start

---
+++ src/MainPackage/index.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage;
+import com.github.freva.asciitable.AsciiTable;
+import java.util.Scanner;
+/**
+ *
+ * @author USER
+ */
+public class index {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Text Color
+    public static final String YELLOW = "\u001B[33m";   // YELLOW
+    
+    public static void main(String[] args) {
+        int pilihan;
+        String jawaban;
+        boolean on = true;
+            
+        //Input Scannser
+        Scanner input = new Scanner(System.in);
+        
+        do{
+            System.out.println("╔══════════════════════════════╗");
+            System.out.println(YELLOW+"║ Sistem Informasi Keluar Masuk Barang Inventory ║");
+            System.out.println("╚══════════════════════════════╝");
+            
+        }
+        while(on == true);
+        }
+    }
+    
+}
